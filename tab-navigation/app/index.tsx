8f6d58bf2c6b87b71b5b530f3eed4d20b6c6d07a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-10-20 14:29:42 +0200

    feat : tab navigation

---
+++ tab-navigation/app/index.tsx
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import MainTabs from './navigation/MainTabs';
+
+const Page = () => {
+  return <MainTabs />;
+};
+
+export default Page;
---
+++ tab-navigation/app/navigation/MainTabs.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export type RootTabParamList = {
+  Home: undefined;
+  Search: undefined;
+  Profile: undefined;
+};
+
+const Tab = createBottomTabNavigator<RootTabParamList>();
+
+const MainTabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: string = '';
+          if (route.name === 'Home') iconName = focused ? 'home' : 'home-outline';
+          else if (route.name === 'Search') iconName = focused ? 'search' : 'search-outline';
+          else if (route.name === 'Profile') iconName = focused ? 'person' : 'person-outline';
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007AFF',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default MainTabs;
